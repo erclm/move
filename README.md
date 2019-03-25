ade71d3b11d67efe32926b3eb7e871600cc91023
Eric Lim <jilim@chapman.edu>
2019-03-25 14:03:15 -0700

    fix auth issues

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ server transfer script for ggservers
 
 ## usage: ./move.sh oldPanelID newNodeName oldServerID newServerID
 operations: ssh, zip, exit, scp, scp, ssh, unzip, chown -R, chmod -R, exit
-## usage example: ./move.sh 103 d2 1034 4
+### usage example: ./move.sh 103 d2 1034 4
+
+## UPDATE:
+### 1.1
+- now keeps old files, fixed authentication issues
--- move.sh
+++ move.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# script by eric lim
+# ver. 1.1
+
 ### $1 = from host
 ### $2 = to host
 ### $3 = oldid
@@ -10,45 +13,56 @@
 ## usage: ./move.sh oldPanelID newNodeName oldServerID newServerID
 ## usage example: ./move.sh 103 d2 1034 4
 
-echo "movig server$3 from mc$1 to server$4 at $2.ggn.io"
-echo "continue? [y/n]"
+echo "*** moving server$3 from mc$1 to server$4 at $2.ggn.io"
+echo "** continue? [y/n]"
 read ans
 if [ "$ans" = "y" ]
 then
-  echo "continueing"
+  echo "*** continuing"
 else
-  echo "exiting"
+  echo "*** exiting"
   exit 1
 fi
 
-echo "connecting to mc$1.ggservers.com.."
+echo "*** connecting to mc$1.ggservers.com.."
 ## zip
 ssh root@mc$1.ggservers.com /bin/bash << EOF
   cd /home/minecraft/multicraft/servers/
+  echo "* cleaning up server$3.."
+  rm -rf server$3/*.zip
+  rm -rf server$3/*.log
+  rm -rf server$3/logs
+  rm -rf server$3/crash-reports
   ls server$3
-  zip -r server$4.zip server$3
-  rm -rf server$3
+  echo "* zipping.."
+  zip -r server$3.zip server$3
 EOF
 
-echo "copying zip to new node.."
-scp -r root@mc$1.ggservers.com:/home/minecraft/multicraft/servers/server$4.zip root@$2.ggn.io:/home/minecraft/multicraft/servers/
+echo "*** copying zip to new node.."
+scp -r root@mc$1.ggservers.com:/home/minecraft/multicraft/servers/server$3.zip ~/server$4.zip
+scp -r ~/server$4.zip root@$2.ggn.io:/home/minecraft/multicraft/servers/server$4.zip
 
-echo "applying changes to new node.."
+echo "*** applying changes to new node.."
 ssh root@$2.ggn.io /bin/bash << EOF
   cd /home/minecraft/multicraft/servers
   rm -rf server$4
+  echo "* unzipping.."
   unzip server$4.zip
+  echo "* setting permissions.."
   chown -R mc$4:mc$4 server$4
   chmod -R 700 server$4
+  echo "* cleaning up.."
   rm -rf server$4.zip
   ls server$4
 EOF
 
-echo "removing old files.."
+echo "*** removing old files.."
 ssh root@mc$1.ggservers.com /bin/bash << EOF
   cd /home/minecraft/multicraft/servers/
-  rm -rf server$4.zip
+  rm -rf server$3.zip
 EOF
 
-echo "operation complete"
+rm -rf server$4.zip
+
+echo "*** operation complete"
 exit 0
